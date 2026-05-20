cb4464bdab26924cbaf987f246a909be17b8cae8
Gabriel Argimon Gonzalez <143468348+Gabrielgonzalez7@users.noreply.github.com>
2026-05-20 09:11:03 -0300

    Add files via upload

---
+++ Deadlock/src/deadlock/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deadlock;
+
+/**
+ *
+ * @author gabri
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Recurso teclado =
+                    new Recurso(" Teclado");
+        Recurso impressora =
+                new Recurso(" Impressora");
+        
+        Processo1 p1=
+                new Processo1(teclado, impressora);
+        
+        Processo2 p2= 
+                new Processo2(teclado, impressora);
+        
+        p1.start();
+        p2.start();
+    }
+    
+}
---
+++ Deadlock/src/deadlock/Recurso.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deadlock;
+
+/**
+ *
+ * @author gabri
+ */
+public class Recurso {
+  private String nome;
+public Recurso(String nome){
+    this.nome = nome;
+}  
+public String getNome(){
+    return nome;
+}
+}
